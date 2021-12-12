da4be9b0ce736edffb1d4df58f49dfbea310ecdc
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2021-12-12 23:43:36 +0100

    checks

--- src/Horostory.API/RegisterMapRoutes.cs
+++ src/Horostory.API/RegisterMapRoutes.cs
@@ -17,12 +17,36 @@ namespace Horostory.API
          
         private static void MapHoroscope(IEndpointRouteBuilder builder)
         {
+            var signs = new List<string>();
+            signs.Add("aries");
+            signs.Add("taurus");
+            signs.Add("gemini");
+            signs.Add("cancer");
+            signs.Add("leo");
+            signs.Add("virgo");
+            signs.Add("libra");
+            signs.Add("scorpio");
+            signs.Add("sagittarius");
+            signs.Add("capricorn");
+            signs.Add("pisces");
+            signs.Add("aquarius");
+
             builder.MapGet("horoscope", async (string sign, string date, IHorostoryService service) =>
             {
                 if (string.IsNullOrWhiteSpace(sign))
                 {
                     return Results.BadRequest();
                 }
+                if (!signs.Contains(sign.ToLower()))
+                {
+                    return Results.BadRequest("Sign not found");
+                }
+
+                if (string.IsNullOrWhiteSpace(date))
+                {
+                    date = "today";
+                }
+
                 return Results.Ok(await service.GetHoroscopeInfo(sign, date));
             })
             .Produces<HoroscopeResponse>(StatusCodes.Status200OK)
